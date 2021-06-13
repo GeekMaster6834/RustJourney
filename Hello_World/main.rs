9caef081756c822f5f6b9670506fb7bd1489834a
GeekMaster6834 <rycgale@gmail.com>
2021-06-13 19:25:07 -0400

    hello world implementation

---
+++ Hello_World/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // "!" represents a macro being used to print output
+    // if calling a function instead, the "!" is removed
+    println!("Hello, world!");
+}
+
+// the program is compiled using "rustc <filename>"
+// rustc is good for simple programs, large programs would be better utilized using cargo
